8b849efa46a5e74b8896a859859bc988d08f8076
Jakub Hozak <jakub.hozak@gmail.com>
2011-03-27 01:34:58 +0100

    fixtures moved

--- test/unit/lib/import/sci_importer_test.rb
+++ test/unit/lib/import/sci_importer_test.rb
@@ -4,6 +4,13 @@ require 'test/test_helper'
 module Import
   class SciImporterTest < ActiveSupport::TestCase
     context "SciImporter" do
+
+      setup do
+        # TODO - use factory
+        Country.find_by_code('AT').update_attribute(:name_en, 'Austria')
+      end
+
+
       should 'handle correct file ' do
         wcs = SciImporter.new(File.new('test/fixtures/sci/2011.csv')).import!
         assert_equal 2, wcs.size
