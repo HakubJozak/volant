c2f0bc0f6a05e6badbe9fa3ac0afc9262fcdd848
Josef Šimánek <retro@ballgag.cz>
2012-11-05 14:42:37 +0100

    Initial bundler support

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source :rubygems
+
+gem "rails", "~> 2.3"
+
+gem "pg"
+gem "rack", "1.0.1"
+gem "will_paginate", "~> 2.0"
+gem 'htmlentities'
+gem 'fastercsv'
+gem 'haml'
+gem 'differ'
+gem "rdoc"
--- config/environment.rb
+++ config/environment.rb
@@ -24,20 +24,6 @@ Rails::Initializer.run do |config|
   # you must remove the Active Record framework.
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
 
-  # Specify gems that this application depends on.
-  # They can then be installed with "rake gems:install" on new installations.
-  # You have to specify the :lib option for libraries, where the Gem name (sqlite3-ruby) differs from the file itself (sqlite3)
-  # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  # config.gem "aws-s3", :lib => "aws/s3"
-
-  config.gem "will_paginate"
-  config.gem 'htmlentities'
-  config.gem 'fastercsv'
-  config.gem 'haml'
-  config.gem 'differ'
-
   # Only load the plugins named here, in the order given. By default, all plugins
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
