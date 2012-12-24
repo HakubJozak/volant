d1404eb31b55837ee6a2a398ce65a69a435532b2
Josef Šimánek <retro@ballgag.cz>
2012-12-24 01:11:38 +0100

    Migrate all users to correct locales.

--- db/migrate/20100204103640_rename_cz_columns_to_cs.rb
+++ db/migrate/20100204103640_rename_cz_columns_to_cs.rb
@@ -3,11 +3,13 @@ class RenameCzColumnsToCs < ActiveRecord::Migration
     rename_column :countries, :name_cz, :name_cs
     rename_column :languages, :name_cz, :name_cs
     rename_column :workcamp_intentions, :description_cz, :description_cs
+    User.find_all_by_locale('cz').each {|u| u.update_attribute(:locale, 'cs')}
   end
 
   def self.down
     rename_column :countries, :name_cs, :name_cz
     rename_column :languages, :name_cs, :name_cz
     rename_column :workcamp_intentions, :description_cs, :description_cz
+    User.find_all_by_locale('cs').each {|u| u.update_attribute(:locale, 'cz')}
   end
 end
