740cec6676e025b8b6500052841f2ce37b7fc944
Jakub Hozak <jakub.hozak@gmail.com>
2014-10-20 13:44:38 +0200

    extracting action buttons

--- test/controllers/email_contacts_controller_test.rb
+++ test/controllers/email_contacts_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class EmailContactsControllerTest < ActionController::TestCase
 
   setup do
-    @email_contact = email_contacts(:one)
+    @email_contact = email_contacts(:seeds)
     sign_in users(:john)
   end
 
@@ -13,39 +13,5 @@ class EmailContactsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:email_contacts)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
 
-  test "should create email_contact" do
-    assert_difference('EmailContact.count') do
-      post :create, email_contact: {  }
-    end
-
-    assert_redirected_to email_contact_path(assigns(:email_contact))
-  end
-
-  test "should show email_contact" do
-    get :show, id: @email_contact
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @email_contact
-    assert_response :success
-  end
-
-  test "should update email_contact" do
-    patch :update, id: @email_contact, email_contact: {  }
-    assert_redirected_to email_contact_path(assigns(:email_contact))
-  end
-
-  test "should destroy email_contact" do
-    assert_difference('EmailContact.count', -1) do
-      delete :destroy, id: @email_contact
-    end
-
-    assert_redirected_to email_contacts_path
-  end
 end
