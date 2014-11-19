273fb4b01a5db650418fc7a0f41210ff1a441832
Jakub Hozak <jakub.hozak@gmail.com>
2014-11-20 00:02:34 +0100

    new button leading to a route

--- app/assets/javascripts/models/country.js.coffee
+++ app/assets/javascripts/models/country.js.coffee
@@ -1,5 +1,8 @@
 Volant.Country = DS.Model.extend
-  name: DS.attr 'string'
+  name_en: DS.attr 'string'
+  name_cz: DS.attr 'string'
+  name: Ember.computed.alias('name_en')
   code: DS.attr 'string'
   triple_code: DS.attr 'string'
+
 #  region: DS.attr 'string'
--- app/serializers/country_serializer.rb
+++ app/serializers/country_serializer.rb
@@ -1,13 +1,8 @@
 class CountrySerializer < ActiveModel::Serializer
-  attributes :id, :name, :code, :triple_code
-
-  def name
-    # hard-wired English for now
-    object.name_en
-  end
+  attributes :id, :name_en, :name_cz, :code, :triple_code
 
   def self.public_attributes
-   [ :name, :code, :triple_code ]
+   [ :name_en, :name_cz, :code, :triple_code ]
   end
 
   def self.private_attributes
