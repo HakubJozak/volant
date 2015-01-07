8144d913cef1b79772582f80c2f5dc4fb5c5c38c
Jakub Hozak <jakub.hozak@gmail.com>
2015-01-07 11:13:30 +0100

    [production] secrets.yml are in shared

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :branch, 'ember'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'log']
+set :shared_paths, ['config/database.yml', 'config/secrets.yml','log']
 
 # Optional settings:
 
@@ -53,12 +53,6 @@ end
 task :setup => :environment do
   queue! %[mkdir -p "#{deploy_to}/#{shared_path}/log"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log"]
-
-  # queue! %[mkdir -p "#{deploy_to}/#{shared_path}/tmp/sockets"]
-  # queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/tmp/sockets"]
-
-  # queue! %[mkdir -p "#{deploy_to}/#{shared_path}/tmp/pids"]
-  # queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/tmp/pids"]
 
   queue! %[mkdir -p "#{deploy_to}/#{shared_path}/config"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config"]
