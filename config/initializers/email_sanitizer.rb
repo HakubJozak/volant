a6b4337d896929d44bf629b1ffb0d03c7d289c2f
Jakub Hozak <jakub.hozak@gmail.com>
2015-01-07 11:54:04 +0100

    adding 'me' to sanitizer

--- config/initializers/email_sanitizer.rb
+++ config/initializers/email_sanitizer.rb
@@ -1,6 +1,6 @@
 class SanitizeEmailInterceptor
   def self.delivering_email(message)
-    message.to = ['info@inexsda.cz']
+    message.to = ['info@inexsda.cz','jakub.hozak@gmail.com']
     message.subject = "[testing] #{message.subject}"
   end
 end
