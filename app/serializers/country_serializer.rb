04dddb093465b951e9996c02bfb31857eb4f0365
Jakub Hozak <jakub.hozak@gmail.com>
2015-01-12 10:12:46 +0100

    minors

--- app/serializers/country_serializer.rb
+++ app/serializers/country_serializer.rb
@@ -1,5 +1,4 @@
 class CountrySerializer < ApplicationSerializer
   has_one :country_zone, embed: :ids, include: true
-  writable_attributes :name_en, :name_cz, :code, :triple_code, :region
-  readonly_attributes :id
+  attributes :name_en, :name_cz, :code, :triple_code, :region, :id
 end
