6f2b17493450bd9b69e1d67ab253fb7c869d33c8
Jakub Hozak <jakub.hozak@gmail.com>
2015-01-19 11:52:32 +0100

    countries api test

--- test/controllers/v1/countries_controller_test.rb
+++ test/controllers/v1/countries_controller_test.rb
@@ -2,10 +2,15 @@ require 'test_helper'
 
 class V1::CountriesControllerTest < ActionController::TestCase
   test 'index' do
+    Factory(:outgoing_workcamp, publish_mode: 'ALWAYS',country: countries(:AT))
+
     get :index
+    
     assert_response :success
     json = json_response['countries'].first
     assert_equal 'Austria', json['name']
+    assert_equal 1, json['workcamps_count']
+    assert_equal 0, json['ltv_count']        
     assert_equal 'Europe Western',json['country_zone_name']
   end
 end
