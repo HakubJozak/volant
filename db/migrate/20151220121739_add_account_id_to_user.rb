2e07b0d4b9a6b24ef79dc8b3a00da935badf24ae
Jakub Hozak <jakub.hozak@gmail.com>
2015-12-20 15:59:58 +0100

    F migration 2

--- db/migrate/20151220121739_add_account_id_to_user.rb
+++ db/migrate/20151220121739_add_account_id_to_user.rb
@@ -1,8 +1,7 @@
 class AddAccountIdToUser < ActiveRecord::Migration
   def change
-    add_column :users, :account_id, :integer
+    add_column :devise_users, :account_id, :integer
     User.reset_column_information
     User.update_all account_id: Account.first.id
-
   end
 end
