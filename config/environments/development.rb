4a412cf8b9f4fc66b07819cd91d969fd1c45a28c
Jakub Hozak <jakub.hozak@gmail.com>
2019-03-22 23:28:06 +0100

    use mailcatcher in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,9 +16,17 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'volant.local' }
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 
+  config.action_mailer.smtp_settings = {
+    authentication: nil,
+    address: "localhost",
+    port: 1025,
+    tls: false,
+    enable_starttls_auto: false,
+  }
+  
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
