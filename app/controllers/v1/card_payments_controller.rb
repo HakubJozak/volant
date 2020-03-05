91313dc824b24251a5e837373bed9cc75a59d7c4
Jakub Hozak <jakub.hozak@gmail.com>
2020-03-05 10:52:28 +0100

    2-spaces formatting in card_payments_controller

--- app/controllers/v1/card_payments_controller.rb
+++ app/controllers/v1/card_payments_controller.rb
@@ -1,26 +1,28 @@
 class V1::CardPaymentsController < V1::BaseController
-	respond_to :json
-	skip_before_action :verify_authenticity_token
+  respond_to :json
+  skip_before_action :verify_authenticity_token
 
-	def create
-		if params[:status] == "PAID"
-			apply_form = ApplyForm.select(:id).find_by_slug(params[:refId])
-			if apply_form
-				Payment.create(apply_form_id: apply_form.id, amount: params[:price].to_f / 100, received: DateTime.now.to_date, description: "Platba kartou", mean: "CARD", external_id: params[:transId])
-			else
-				render json: { message: "No apply form found" }, status: :not_found
-			end
-		end
+  def create
+    if params[:status] == "PAID"
+      apply_form = ApplyForm.select(:id).find_by_slug(params[:refId])
 
-		render json: { message: "OK" }, status: :ok
-	end
+      if apply_form
+	Payment.create(apply_form_id: apply_form.id, amount: params[:price].to_f / 100, received: DateTime.now.to_date, description: "Platba kartou", mean: "CARD", external_id: params[:transId])
+      else
+	render json: { message: "No apply form found" }, status: :not_found
+      end
+    end
 
-	def show
-		apply_form = ApplyForm.select(:fee).find_by_slug(params[:id])
-		if apply_form
-			render json: { fee: apply_form.fee.to_f }
-		else
-			render json: { message: "No apply form found" }, status: :not_found
-		end
-	end
+    render json: { message: "OK" }, status: :ok
+  end
+
+  def show
+    apply_form = ApplyForm.select(:fee).find_by_slug(params[:id])
+
+    if apply_form
+      render json: { fee: apply_form.fee.to_f }
+    else
+      render json: { message: "No apply form found" }, status: :not_found
+    end
+  end
 end
